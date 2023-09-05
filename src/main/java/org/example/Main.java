c02c6edda7be1c41d663af1b5b11058f88093d85
antara-walid <antara.walid.27@gmail.com>
2023-09-05 16:56:59 +0100

    set up hibernate programmatically using PersistanceUnitInfo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,18 +4,30 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import org.example.entities.Student;
+import org.example.persistence.MyPersistenceUnitInfo;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+
+        // 1. Create EntityManagerFactory based on the persistence.xml file
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+
+        // 2 Create EntityManagerFactory using the persistence unit info
+        EntityManagerFactory emf = new HibernatePersistenceProvider().createContainerEntityManagerFactory(
+                new MyPersistenceUnitInfo(),
+                new HashMap<>()
+        );
 
         EntityManager entityManager = emf.createEntityManager(); // through the entity manager we can operate on the context
 
         try{
             entityManager.getTransaction().begin();
             Student student = new Student();
-            student.setId(1L);
-            student.setName("hello");
+            student.setId(2L);
+            student.setName("world");
 
             entityManager.persist(student);
             entityManager.getTransaction().commit();
